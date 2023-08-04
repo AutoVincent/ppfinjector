c78d0f836bac47b8f3d7ff6c440cdb96c9ef1648
AutoVincent <72301871+AutoVincent@users.noreply.github.com>
2023-08-04 23:54:57 +0100

    ppfinjector#3: Make ppfinjector hook CreateFileW
    * Allocate a console and write the file name out.

--- app/ppfinjector/dllmain.cpp
+++ app/ppfinjector/dllmain.cpp
@@ -1,17 +1,67 @@
+#include <iostream>
+
 #include <Windows.h>
 
-BOOL APIENTRY DllMain( HMODULE,
-                       DWORD  ul_reason_for_call,
-                       LPVOID)
+#include <detours/detours.h>
+
+namespace {
+
+   auto g_origCreateFileW = CreateFileW;
+
+   HANDLE WINAPI CreateFileWHook(
+      _In_ LPCWSTR lpFileName,
+      _In_ DWORD dwDesiredAccess,
+      _In_ DWORD dwShareMode,
+      _In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes,
+      _In_ DWORD dwCreationDisposition,
+      _In_ DWORD dwFlagsAndAttributes,
+      _In_opt_ HANDLE hTemplateFile)
+   {
+      static const auto kConsole = AllocConsole();
+
+      std::wcout << "Opening: " << lpFileName << std::endl;
+
+      return g_origCreateFileW(
+         lpFileName,
+         dwDesiredAccess,
+         dwShareMode,
+         lpSecurityAttributes,
+         dwCreationDisposition,
+         dwFlagsAndAttributes,
+         hTemplateFile);
+   }
+
+}
+
+BOOL APIENTRY DllMain(HMODULE, DWORD reason, LPVOID)
 {
-    switch (ul_reason_for_call)
+   if (DetourIsHelperProcess()) {
+      return TRUE;
+   }
+
+    switch (reason)
     {
     case DLL_PROCESS_ATTACH:
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
+       DetourRestoreAfterWith();
+       DetourTransactionBegin();
+       DetourUpdateThread(GetCurrentThread());
+       
+       DetourAttach(
+          reinterpret_cast<PVOID*>(&g_origCreateFileW),
+          CreateFileWHook);
+
+       DetourTransactionCommit();
+       break;
     case DLL_PROCESS_DETACH:
+       DetourTransactionBegin();
+       DetourUpdateThread(GetCurrentThread());
+       DetourDetach(
+          reinterpret_cast<PVOID*>(&g_origCreateFileW),
+          CreateFileWHook);
+       DetourTransactionCommit();
         break;
     }
+
     return TRUE;
 }
 
